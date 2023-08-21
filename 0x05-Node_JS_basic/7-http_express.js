260562f10d44458c7d73e07d76e06ba6b99f5693
Carlos Wilson Niqx <wkabiru19@gmail.com>
2023-08-21 19:44:18 +0300

    Update 7-http_express.js

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,26 +1,29 @@
-const http = require('http');
+const express = require('express');
 const students = require('./3-read_file_async');
+const app = express();
 const hostname = '127.0.0.1';
 const port = 1245;
 
-const app = http.createServer((req, res) => {
+app.get('/', (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  if (req.url === '/') {
-    res.end('Hello Holberton School!');
-  } else if (req.url === '/students') {
-    res.write('This is the list of our students\n');
-    students(process.argv[2]).then((data) => {
-      res.write(`Number of students: ${data.students.length}\n`);
-      res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
-      res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}`);
-      res.end();
-    }).catch((err) => res.end(err.message));
-  }
-});
-  
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}`);
+  res.send('Hello Holberton School!');
 });
 
-module.exports = app;
+app.get('/students', async (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.write('This is the list of our students\n');
+  await students(process.argv[2]).then((data) => {
+    res.write(`Number of students: ${data.students.length}\n`);
+    res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
+    res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}`);
+  }).catch((err) => res.write(err.message))
+    .finally(() => {
+      res.end();
+    });
+});
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}`);
+});
